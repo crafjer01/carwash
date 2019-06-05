3e29e3c6006e04affe267f518308b399f2154e3d
crafjer01 <eduardopineda06@gmail.com>
2019-06-05 17:27:45 -0400

    Configuracion de los JS y SCSS de nuestra aplicacion y la configuracion de la base de datos

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -9,8 +9,6 @@ window._ = require('lodash');
 
 try {
     window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
 } catch (e) {}
 
 /**
@@ -19,7 +17,7 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+//window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
